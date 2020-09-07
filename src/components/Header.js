a12e558e13c493b91aadb5f845945008919db573
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-07 16:32:11 +0400

    feat: add Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../assets/stylesheets/Header.css";
+
+const TheHeader = () =>{
+  return (
+    <header className="page-header">
+      <h1 className="page__header">Exchange Rates</h1>
+    </header>
+  )
+}
+
+export default TheHeader;
