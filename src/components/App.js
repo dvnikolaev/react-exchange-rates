67eda486d908e32ecd12f9937784f51642c330cf
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-07 16:53:45 +0400

    feat: add main component

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import "../assets/stylesheets/Normalize.css";
 import "../assets/stylesheets/App.css";
 import Header from "./Header";
+import Main from "./Main";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 };
