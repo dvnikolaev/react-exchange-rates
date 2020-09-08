1456c2b8892633b49dd08958f59735548f819fc3
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-08 11:59:39 +0400

    feat: add historyWorths

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "../assets/stylesheets/Main.css";
 import BaseRate from "./Base/BaseRate";
+import HistoryWorths from "./HistoryWorths";
 
 const Main = () => {
   return (
     <main className="page-main">
       <BaseRate />
+      <HistoryWorths />
     </main>
   );
 };
