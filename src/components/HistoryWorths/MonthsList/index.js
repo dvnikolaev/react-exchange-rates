be4ca12455ee91cf5195f61d4e29e23af9682303
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-09 10:34:11 +0400

    feat: add activeMonth, renderListItem

--- src/components/HistoryWorths/MonthsList/index.js
+++ src/components/HistoryWorths/MonthsList/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MonthsListItem from "./MonthsListItem";
+import { getMonthsName } from "../../../services/";
 
 const MonthList = () => {
-  return (
-    <ul>
-      <MonthsListItem />
-      MonthList
-    </ul>
-  );
+  const [activeMonth, setActiveMonth] = useState(3);
+
+  const monthsNames = [
+    getMonthsName(new Date().getMonth() - 4),
+    getMonthsName(new Date().getMonth() - 3),
+    getMonthsName(new Date().getMonth() - 2),
+    getMonthsName(new Date().getMonth() - 1),
+  ];
+
+  const renderListItem = monthsNames.map((name, index) => {
+    return (
+      <MonthsListItem
+        name={name}
+        key={name}
+        index={index}
+        activeMonth={activeMonth}
+        setActiveMonth={setActiveMonth}
+      />
+    );
+  });
+
+  return <ul className="months-list">{renderListItem}</ul>;
 };
 
 export default MonthList;
