8ceee1959f7fee1b29be14392acb981183627631
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-09 15:07:14 +0400

    feat: add fetch history worths

--- src/store/reducers/rates.js
+++ src/store/reducers/rates.js
@@ -1,8 +1,9 @@
-import { FETCH_RATES } from "../actions/types";
+import { FETCH_RATES, FETCH_HISTORY_WORTHS } from "../actions/types";
 
 const INITIAL_STATE = {
   baseRate: 'USD',
   secondaryRates: ["RUB", "EUR", "CHF","JPY"],
+  historyWorths: [],
   rates: [],
 };
 
@@ -10,6 +11,9 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_RATES: {
       return { ...state, rates: action.payload };
+    }
+    case FETCH_HISTORY_WORTHS: {
+      return {...state, historyWorths: action.payload}
     }
     default:
       return state;
