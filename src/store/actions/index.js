1c38596da84cb111541394835149dbbc86fc9887
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-09 15:08:30 +0400

    feat: add fetch history worths

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,8 +1,18 @@
-import { FETCH_RATES } from './types';
+import { FETCH_RATES, FETCH_HISTORY_WORTHS } from "./types";
+import { getRatesForLastFourMonths } from "../../services";
 
 export const fetchRates = (id) => {
   return {
     type: FETCH_RATES,
-    payload: id
-  }
-}
+    payload: id,
+  };
+};
+
+export const fetchHistoryWorths = (baseRate) => async (dispatch) => {
+  const historyWorths = await getRatesForLastFourMonths(baseRate);
+
+  dispatch({
+    type: FETCH_HISTORY_WORTHS,
+    payload: historyWorths,
+  });
+};
