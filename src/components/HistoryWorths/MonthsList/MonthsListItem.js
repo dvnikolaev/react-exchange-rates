5d4428395deeebc9facb7a0694e45938e7cd2335
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-09 10:35:27 +0400

    feat: add month list item

--- src/components/HistoryWorths/MonthsList/MonthsListItem.js
+++ src/components/HistoryWorths/MonthsList/MonthsListItem.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const MonthsListItem = () => {
-  return <h2>MonthsListItem</h2>
-}
+const MonthsListItem = ({ name, index, activeMonth, setActiveMonth }) => {
+  const renderClassNameActive = () => {
+    return index === activeMonth ? "months-list__button--active" : "";
+  };
+
+  return (
+    <li className="months-list__item">
+      <button
+        className={`months-list__button ${renderClassNameActive()}`}
+        onClick={() => setActiveMonth(index)}
+      >
+        {name}
+      </button>
+    </li>
+  );
+};
 
 export default MonthsListItem;
