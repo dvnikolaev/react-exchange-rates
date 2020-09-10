48d21f0d2c77912956e9bbff7d1b85dd556b9266
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-10 09:10:35 +0400

    feat: add rates

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
 import "../assets/stylesheets/Main.css";
+import { fetchRates } from "../store/actions";
 import BaseRate from "./Base/BaseRate";
 import HistoryWorths from "./HistoryWorths";
+import Rates from "./Rates";
 
-const Main = () => {
+const Main = ({ fetchRates }) => {
+  useEffect(() => {
+    fetchRates();
+  }, []);
+
   return (
     <main className="page-main">
       <BaseRate />
       <HistoryWorths />
+      <Rates />
     </main>
   );
 };
 
-export default Main;
+export default connect(null, { fetchRates })(Main);
