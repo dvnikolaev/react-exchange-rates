6534e3ce433fbc9fcfbd948bca941a2e98d2b1b4
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-10 09:12:20 +0400

    feat: add selectBaseRate

--- src/store/reducers/rates.js
+++ src/store/reducers/rates.js
@@ -1,8 +1,12 @@
-import { FETCH_RATES, FETCH_HISTORY_WORTHS } from "../actions/types";
+import {
+  FETCH_RATES,
+  FETCH_HISTORY_WORTHS,
+  SELECT_BASE_RATE,
+} from "../actions/types";
 
 const INITIAL_STATE = {
-  baseRate: 'USD',
-  secondaryRates: ["RUB", "EUR", "CHF","JPY"],
+  baseRate: "USD",
+  secondaryRates: ["RUB", "EUR", "CHF", "JPY"],
   historyWorths: [],
   rates: [],
 };
@@ -13,7 +17,10 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, rates: action.payload };
     }
     case FETCH_HISTORY_WORTHS: {
-      return {...state, historyWorths: action.payload}
+      return { ...state, historyWorths: action.payload };
+    }
+    case SELECT_BASE_RATE: {
+      return { ...state, baseRate: action.payload };
     }
     default:
       return state;
