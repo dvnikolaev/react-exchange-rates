c3df7e9f499267bcd045b1402ae2785b988b23f8
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-22 10:48:40 +0400

    add get rates full month

--- src/services/index.js
+++ src/services/index.js
@@ -29,3 +29,38 @@ export const getRatesForLastFourMonths = async (
 
   return [firstMonth, secondsMonth, thirdMonth, fourthMonth]
 };
+
+export const getRatesFullCurrentMonth = async (base, symbols) => {
+  const symbolsString = symbols.join(',');
+  const startDate = new Date(new Date().getFullYear(), new Date().getMonth(), 2).toISOString().substr(0,10);
+  const endDate = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1).toISOString().substr(0,10);
+
+  const { data: { rates }} = await axios.get(`https://api.exchangerate.host/timeseries?start_date=${startDate}&end_date=${endDate}&base=${base}&symbols=${symbolsString}`);
+  const data = Object.values(rates);
+  let arr1 = [];
+  let arr2 = [];
+  let arr3 = [];
+  let arr4 = [];
+  
+  for (let i = 0; i < data.length; i++) {
+    for (let key in data[i]) {
+      switch(key) {
+        case symbols[0]:
+          arr1.push(data[i][key]);
+          break;
+        case symbols[1]:
+          arr2.push(data[i][key]);
+          break;
+        case symbols[2]:
+          arr3.push(data[i][key]);
+          break;
+        case symbols[3]:
+          arr4.push(data[i][key]);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return [arr1,arr2,arr3,arr4];
+}
