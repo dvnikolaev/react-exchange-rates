02830ec49dddba664e87fd24627835e6de099082
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-23 11:23:11 +0400

    add composeWithDevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = createStore(reducers,composeEnhancers(applyMiddleware(thunk)))
+export const store = createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
