23ac550738033bf074cbcb56225ea04dc120f4c5
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-23 11:26:58 +0400

    Updates

---
+++ precache-manifest.58b170940837634a32f17a9e83a699a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f106bf1f647cb701a4d47d780c2bbb8",
+    "url": "/react-exchange-rates/index.html"
+  },
+  {
+    "revision": "51f4f8a55eec01284cba",
+    "url": "/react-exchange-rates/static/css/main.8dfd56f9.chunk.css"
+  },
+  {
+    "revision": "42ae7417919f1922e232",
+    "url": "/react-exchange-rates/static/js/2.76214d15.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/react-exchange-rates/static/js/2.76214d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f4f8a55eec01284cba",
+    "url": "/react-exchange-rates/static/js/main.795583e6.chunk.js"
+  },
+  {
+    "revision": "29dc04278f35716379ff",
+    "url": "/react-exchange-rates/static/js/runtime-main.0ab15408.js"
+  }
+]);
